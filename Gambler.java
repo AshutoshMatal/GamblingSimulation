395ef49933992be316d93811385e71c54c6ffa68
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-02-27 17:48:02 +0530

    ADDED :Start with a Stake of 00 everyday and bet  every game

--- Gambler.java
+++ Gambler.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
 
 public class Gambler {
 	public static void main(String[] args) {
+		int bet=1;
+		int stake=100;
 		System.out.println("Welcome to Gambler Simulation");
+		
+		
 		// TODO Auto-generated method stub
 
 	}
