048d00d3d92068df2883ac2250e387a2da4ced99
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-02-28 12:36:29 +0530

    MODIFIED:Twenty days of playing everyday like to know the Total amount of won or lost

--- Gambler.java
+++ Gambler.java
@@ -6,30 +6,47 @@ public class Gambler {
 	public static void main(String[] args) {
 		int bets=1;
 		int cash=100;  
-		int win=0;
-		int lost=0;
+		
 		int result=0;
-		int number=1000;
+		int number=20;
+		int totalamount=0;
+		int counter=1;
 		System.out.println("Welcome to Gambler Simulation");
 	    // BY USING FOR LOOP COUNT TILL THE NUMBER
-		for (int count=1;count<number; count++) 
+		for (int day=1;day<=number; day++) 
 		{	
+		int win=0;
+		int lost=0;
+			for(int game=1;game<=100;game++)
+			{
 				if (Math.random() < 0.5) 
-				{
+				{ 
 					result=cash+bets;
 					win++;
-					win=cash+50;				// IF IT WILL WON 50% THAN CASH THEN IT WILL RESIGN
-					break;
+					System.out.println("win :"+win);
+					if(win==50)	
+					{
+						break;						// IF IT WILL WON 50% THAN CASH THEN IT WILL RESIGN
+					}
 					
 				} else 
 				{
 					result=cash-bets;
 					lost++;
-					lost=cash-50;				//IF IT WILL LOST 50% THAN CASH THEN IT WILL RESIGN
-					break;
-				}
+					System.out.println("lost:"+lost);
+					if(lost==50)
+					{
+						break;                           //IF IT WILL LOST 50% THAN CASH THEN IT WILL RESIGN		
+					}
+				}	
 			}
-		System.out.println("win :" + win);		// WIN COUNT
-		System.out.println("lost :" +lost);		//LOST COUNT
+			System.out.println( counter++ + ")"  + "perdayamount:"+result); //PERDAY RESULT
+			System.out.println("win per day  :" + win);						// WIN COUNT
+			System.out.println("lost per day :" +lost);						//LOST COUNT
+			
+			totalamount=totalamount+result;
+		}
+		System.out.println( "Total-amount cash  won or lost : "+totalamount);
+		
 	}
 }
