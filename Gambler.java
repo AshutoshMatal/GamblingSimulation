4bb972156802399c574b7b4443be7bf68d43ea66
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-02-28 10:26:38 +0530

    MODIFIED :Gambler make bet 1 so either win or losse

--- Gambler.java
+++ Gambler.java
@@ -1,14 +1,33 @@
+
+import java.util.Random;
 import java.util.Scanner;
 
 public class Gambler {
 	public static void main(String[] args) {
-		int bet=1;
-		int stake=100;
+		int bets=1;
+		int cash=100;  
+		int win=0;
+		int lost=0;
+		int result=0;
 		System.out.println("Welcome to Gambler Simulation");
-		
-		
-		// TODO Auto-generated method stub
-
+	    Scanner sc=new Scanner (System.in);
+		System.out.println("Enter a trials ");
+		int trials = sc.nextInt();
+		for (int day= 1; day<trials; day++) 
+		{	
+				if (Math.random() < 0.5) 
+				{
+					result=cash+bets;
+					win++;
+					
+				} else 
+				{
+					result=cash-bets;
+					lost++;
+				}
+				System.out.println(result);
+			}
+		System.out.println("win :" + win);
+		System.out.println("lost :" +lost);
 	}
-
 }
