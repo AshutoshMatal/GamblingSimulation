f478c240102962d6b0a589cbc4cb663d4bc3c7e8
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-02-28 11:11:18 +0530

    MODIFIED :Won or Lost 50% of stake

--- Gambler.java
+++ Gambler.java
@@ -9,25 +9,27 @@ public class Gambler {
 		int win=0;
 		int lost=0;
 		int result=0;
+		int number=1000;
 		System.out.println("Welcome to Gambler Simulation");
-	    Scanner sc=new Scanner (System.in);
-		System.out.println("Enter a trials ");
-		int trials = sc.nextInt();
-		for (int day= 1; day<trials; day++) 
+	    // BY USING FOR LOOP COUNT TILL THE NUMBER
+		for (int count=1;count<number; count++) 
 		{	
 				if (Math.random() < 0.5) 
 				{
 					result=cash+bets;
 					win++;
+					win=cash+50;				// IF IT WILL WON 50% THAN CASH THEN IT WILL RESIGN
+					break;
 					
 				} else 
 				{
 					result=cash-bets;
 					lost++;
+					lost=cash-50;				//IF IT WILL LOST 50% THAN CASH THEN IT WILL RESIGN
+					break;
 				}
-				System.out.println(result);
 			}
-		System.out.println("win :" + win);
-		System.out.println("lost :" +lost);
+		System.out.println("win :" + win);		// WIN COUNT
+		System.out.println("lost :" +lost);		//LOST COUNT
 	}
 }
