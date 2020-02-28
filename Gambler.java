98cd5a712f8e6087d59f34b02d9ce3431b5a8a03
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-02-28 17:42:35 +0530

    MODIFIED:Luckiest day i won maximum and Unluckiest day i lost maximum

--- Gambler.java
+++ Gambler.java
@@ -1,20 +1,55 @@
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Gambler {
+public class Gambler 
+{
 	int bets=1;
 	int cash=100;  
 	int result=0;
 	int number=30;
 	int totalamount=0;
 	int counter=1;
-	int win=0;
-	int lost=0;
+	static int win=0;
+	static int lost=0;
 	int	perdaywin;
 	int perdaylost;
-	
-		// BY USING FOR LOOP COUNT TILL THE NUMBER
+	static List<Integer> ls=new ArrayList<Integer>();
+	void perdays()
+	{ 
+		for (int day=1;day<=number; day++) 
+		{	
+			 win=0; lost=0;
+			for(int game=1;game<=100;game++)
+			{
+				if (Math.random() < 0.5) 
+				{ 
+					result=cash+bets;
+					win++;
+					//System.out.println("win :"+win);	
+				} 
+				else 
+				{
+					result=cash-bets;
+					lost++;
+					//System.out.println("lost:"+lost);	
+				}	
+			}
+			perdaywin=win;
+			perdaylost=lost;
+			System.out.println( counter++ + ")"+"perdayamount:"+result); //PERDAY RESULT
+			System.out.println("win per day  :" + win);			// WIN COUNT
+			ls.add(win);
+			System.out.println("lost per day :" +lost);		//LOST COUNT
+			ls.add(lost);
+			totalamount=totalamount+result;
+		}		
+}
+	// BY USING FOR LOOP COUNT TILL THE NUMBER
 	void month()
 	{
 		for (int day=1;day<=number; day++) 
@@ -45,25 +80,30 @@ public class Gambler {
 			}
 			perdaywin=win;
 			perdaylost=lost;
-			
-			//System.out.println( counter++ + ")"+"perdayamount:"+result); //PERDAY RESULT
-			//System.out.println("win per day  :" + win);			// WIN COUNT
-			//System.out.println("lost per day :" +lost);			//LOST COUNT
+			/*System.out.println( counter++ + ")"+"perdayamount:"+result); //PERDAY RESULT
+			System.out.println("win per day  :" + win);			// WIN COUNT
+			System.out.println("lost per day :" +lost);		*/	//LOST COUNT
 			totalamount=totalamount+result;
 	}
 	System.out.println("win per month :" + win);				// WIN COUNT PERMONTH
 	System.out.println("lost per month :" +lost);				//LOST COUNT PERMONTH
-	System.out.println( "Total-amount cash  won or lost PerMonth : "+totalamount);
-	
+	System.out.println( "Total-amount cash  won or lost PerMonth : "+totalamount);	
 }
 public static void main(String[] args) {
+	
 	Gambler gm=new Gambler();
 	System.out.println("Welcome to Gambler Simulation");
 	for(int month=1;month<=12;month++)
 	{
 		gm.month();
 	}
-}
+	System.out.println("**********************************************************");
+	gm.perdays();
+	Collections.sort(ls);
+	System.out.println("*************************************************************");
+	System.out.println(" Luckiest day i won Maximum is   :"+Collections.max(ls));
+	System.out.println(" UnLuckiest day i won Minumum is :"+Collections.min(ls));
+	}
 }
 	
 	
